c4a913d6213d7e5a5b0bc049559682949664747a
pnorman411 <2689-pnorman411-yEONlp@users.noreply.umn.bootcampcontent.com>
2022-02-01 20:23:05 -0600

    deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,8 +1,19 @@
 library(dplyr)
 
+#import and read csv as dataframe
 car_table2 <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
 
-lin_mpg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, car_table2) #perform linear regression
+#perform linear regression
+lin_mpg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, car_table2) 
 
-summary(lin_mpg) #use summary function to determine the p-value and r-squared value
+#use summary function to determine the p-value and r-squared value
+summary(lin_mpg) 
 
+#import and read suspension_coil csv as dataframe
+coil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+
+#create summary table for mean, median, variance, and standard deviation
+total_summary <- coil_table %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+#create summary table for mean, median, variance, and standard deviation by grouping by lots
+lot_summary <- coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
